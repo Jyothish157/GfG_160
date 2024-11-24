7c31e4c386bae8aaaabe1d3075a78e0d384eb563
Jyothishkumar V S <128952185+Jyothish157@users.noreply.github.com>
2024-11-24 10:16:45 +0530

    Create reverseArray.py

---
+++ reverseArray.py
@@ -0,0 +1,7 @@
+class Solution:
+    def reverseArray(self, arr):
+        # code here
+        n = len(arr)
+        for i in range(n//2):
+            arr[i],arr[n-i-1] = arr[n-i-1],arr[i]
+        return arr
