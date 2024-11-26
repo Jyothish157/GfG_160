3b256a49e38776adb913a9f5eb53ad9c7acdc914
Jyothishkumar V S <128952185+Jyothish157@users.noreply.github.com>
2024-11-26 22:38:16 +0530

    Create maximumProfit.py

---
+++ maximumProfit.py
@@ -0,0 +1,13 @@
+from typing import List
+
+class Solution:
+    def maximumProfit(self, prices: List[int]) -> int:
+        max_profit = 0
+        n = len(prices)
+        
+        for i in range(1, n):
+
+            if prices[i] > prices[i - 1]:
+                max_profit += prices[i] - prices[i - 1]
+        
+        return max_profit
