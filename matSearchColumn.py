d59ee8bfe3d776d9af65dc9af96c771216729429
Jyothishkumar V S <128952185+Jyothish157@users.noreply.github.com>
2024-12-24 17:32:33 +0530

    Create matSearchColumn.py

---
+++ matSearchColumn.py
@@ -0,0 +1,20 @@
+class Solution:
+	def matSearch(self, mat, x):
+		# Complete this function
+		if not mat or not mat[0]:
+            return False
+        
+        n = len(mat)
+        m = len(mat[0])  
+        
+        row, col = 0, m - 1
+        
+        while row < n and col >= 0:
+            if mat[row][col] == x:
+                return True
+            elif mat[row][col] > x:
+                col -= 1  
+            else:
+                row += 1 
+        
+        return False  
